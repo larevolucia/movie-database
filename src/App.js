b643037d6aa04007455d350f4d20424f102c3ea0
larevolucia <reis.lucia@gmail.com>
2024-06-26 16:02:01 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./styles/App.css";
-import { Route, Routes } from "react-router-dom";
+//import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-//import Results from "./components/Results";
-import About from "./About";
+//import About from "./About";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:query" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <Home />
         <footer>Credits go here.</footer>
       </div>
     </div>
