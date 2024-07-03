7719953efe1f8f6b6fc84008a741be1f84af2692
larevolucia <reis.lucia@gmail.com>
2024-07-03 14:03:54 +0200

    removed navigation from About page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Navigation from "./components/Navigation";
 
 const About = () => {
   return (
     <div>
-      <Navigation />
       <h1>About</h1>
       <p>This is a summary of the project. To be completed</p>
     </div>
