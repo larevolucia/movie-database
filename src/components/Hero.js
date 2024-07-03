f00547e4cb417bf47ebba017f4d55a8935defdbe
larevolucia <reis.lucia@gmail.com>
2024-07-03 14:54:38 +0200

    fixed Hero function name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function Carousel({ data }) {
+export default function Hero({ data }) {
   const navigate = useNavigate();
   console.log(data);
   if (data) {
