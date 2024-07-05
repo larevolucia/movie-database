5ec6925eb6fc3d430001eb2f3fad47a7df259fd0
larevolucia <reis.lucia@gmail.com>
2024-07-05 20:29:22 +0200

    testing styled-components on Details Page

--- src/components/TitleDetails.js
+++ src/components/TitleDetails.js
@@ -1,18 +1,65 @@
 import React from "react";
-//import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+`;
+
+const Poster = styled.img`
+  width: 300px;
+  height: 450px;
+  margin-right: 20px;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  margin: 0;
+`;
+
+const Overview = styled.p`
+  font-size: 1.2rem;
+  line-height: 1.5;
+`;
+
+const Info = styled.div`
+  margin: 10px 0;
+`;
+
+const InfoLabel = styled.span`
+  font-weight: bold;
+`;
 
 function TitleDetails({ mediaType, details }) {
-  // const { media_type } = useParams();
-
   return (
-    <div data-testid="title-details">
-      <h1>{mediaType === "movie" ? details.title : details.name}</h1>
-      <p>{details.overview}</p>
-      <img
+    <Container data-testid="title-details">
+      <Poster
         src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
         alt={mediaType === "movie" ? details.title : details.name}
       />
-    </div>
+      <Details>
+        <Title>{mediaType === "movie" ? details.title : details.name}</Title>
+        <Info>
+          <InfoLabel>Release Date:</InfoLabel> {details.release_date}
+        </Info>
+        <Info>
+          <InfoLabel>Genres:</InfoLabel>{" "}
+          {details.genres.map((genre) => genre.name).join(", ")}
+        </Info>
+        <Info>
+          <InfoLabel>Rating:</InfoLabel> {details.vote_average}
+        </Info>
+        <Overview>{details.overview}</Overview>
+      </Details>
+    </Container>
   );
 }
+
 export default TitleDetails;
