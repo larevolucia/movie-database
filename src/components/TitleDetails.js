dc7f94e8e2944b5cc56929b5b43e7eaa082a5f52
larevolucia <reis.lucia@gmail.com>
2024-07-13 15:13:58 +0200

    Update TitleDetails basic design

--- src/components/TitleDetails.js
+++ src/components/TitleDetails.js
@@ -1,23 +1,34 @@
 import React from "react";
+// import React, { useCallback, useEffect, useState } from "react";
+// import { apiEndpoint, getHeaders } from "../utils/apiConfig";
+import useWindowSize from "../hooks/useWindowSize";
+// import ContentRail from "./ContentRail";
 import styled from "styled-components";
+import DateToYear from "../formatters/DateToYear";
+// import axios from "axios";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 20px;
+const HeaderInfo = styled.ul`
+    align-items: center;
+    display: inline-flex;
+    flex-wrap: wrap;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}`;
 
-  @media (min-width: 768px) {
-    flex-direction: row;
-  }
+const HeaderInfoTag = styled.li`
+    padding: 0 0.5rem;
+}`;
+
+const Header = styled.div`
+  padding: 10px 5px;
 `;
 
 const Poster = styled.img`
-  width: 100%;
   height: auto;
-  margin-bottom: 20px;
+  max-width: 200px;
 
-  @media (min-width: 768px) {
-    width: 300px;
+  @media (min-width: 900px) {
     height: auto;
     margin-right: 20px;
     margin-bottom: 0;
@@ -25,63 +36,86 @@ const Poster = styled.img`
 `;
 
 const Details = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 600px;
-`;
+  display: inline-grid;
+  grid-gap: 1rem;
+  grid-template-columns: 1fr 2fr;
 
-const Title = styled.h1`
-  font-size: 2rem;
-  margin: 0;
-
-  @media (min-width: 768px) {
-    font-size: 2.5rem;
+  @media (min-width: 900px) {
+    grid-template-columns: 0.5fr 2fr 1fr;
   }
 `;
 
-const Overview = styled.p`
-  font-size: 1rem;
-  line-height: 1.5;
-
-  @media (min-width: 768px) {
-    font-size: 1.2rem;
-  }
+const Synopsis = styled.div`
+  width: 100%;
+  padding: 0.5rem;
 `;
 
-const Info = styled.div`
-  margin: 10px 0;
+const MovieInfo = styled.div`
+  padding: 0.5rem;
 `;
 
-const InfoLabel = styled.span`
-  font-weight: bold;
+const Info = styled.p`
+  margin-top: 0.5em;
 `;
 
 function TitleDetails({ mediaType, details }) {
   console.log(details);
+  const windowSize = useWindowSize();
+  const roundedVoteAverage = Math.round(details.vote_average).toFixed(1);
+  console.log(details.vote_average);
+
+  const formattedOverview = details.overview
+    .split("\n\n")
+    .map((paragraph, index) => <p key={index}>{paragraph}</p>);
 
   return (
-    <Container data-testid="title-details">
-      <Poster
-        src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
-        alt={mediaType === "movie" ? details.title : details.name}
-      />
+    <div data-testid="title-details">
+      <Header>
+        <h1>{mediaType === "movie" ? details.title : details.name}</h1>
+        <HeaderInfo>
+          <HeaderInfoTag>{mediaType}</HeaderInfoTag>
+          <HeaderInfoTag>
+            <DateToYear fullDate={details.release_date} />
+          </HeaderInfoTag>
+          <HeaderInfoTag>{details.runtime}m</HeaderInfoTag>
+        </HeaderInfo>
+      </Header>
       <Details>
-        <Title>{mediaType === "movie" ? details.title : details.name}</Title>
-        <Info>
-          <InfoLabel>Release Date:</InfoLabel> {details.release_date}
-        </Info>
-        {details.genres !== undefined ? (
-          <Info>
-            <InfoLabel>Genres:</InfoLabel>{" "}
-            {details.genres.map((genre) => genre.name).join(", ")}
-          </Info>
-        ) : null}
-        <Info>
-          <InfoLabel>Rating:</InfoLabel> {details.vote_average}
-        </Info>
-        <Overview>{details.overview}</Overview>
+        <div>
+          <Poster
+            src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
+            alt={mediaType === "movie" ? details.title : details.name}
+          />
+          {windowSize.width <= 600 && (
+            <MovieInfo>
+              {details.genres !== undefined ? (
+                <Info>
+                  <strong>Genres:</strong>{" "}
+                  {details.genres.map((genre) => genre.name).join(", ")}
+                </Info>
+              ) : null}
+              <Info>
+                <strong>Rating:</strong> {roundedVoteAverage}
+              </Info>
+            </MovieInfo>
+          )}
+        </div>
+        <Synopsis>{formattedOverview}</Synopsis>
+        {windowSize.width > 600 && (
+          <MovieInfo>
+            {details.genres !== undefined ? (
+              <Info>
+                <strong>Genres:</strong>{" "}
+                {details.genres.map((genre) => genre.name).join(", ")}
+              </Info>
+            ) : null}
+            <Info>
+              <strong>Rating:</strong> {roundedVoteAverage}
+            </Info>
+          </MovieInfo>
+        )}
       </Details>
-    </Container>
+    </div>
   );
 }
 
