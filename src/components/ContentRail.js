88631151ddfe19f74841271a118d9bfb42458de6
larevolucia <reis.lucia@gmail.com>
2024-07-16 16:48:48 +0200

    Carousel-like scroll using fontawesome and styled components

--- src/components/ContentRail.js
+++ src/components/ContentRail.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import GroupJobs from "../formatters/GroupJobs";
 import useHandleRowClick from "../utils/useHandleRowClick";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight
+} from "@fortawesome/free-solid-svg-icons";
 
 const Container = styled.div`
   margin: 15px 0;
 `;
+const RailContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const Rail = styled.div`
   display: flex;
-  overflow-x: scroll;
+  overflow-x: hidden;
   padding: 0 10px 20px 10px;
 `;
 const Title = styled.h3`
@@ -46,6 +56,18 @@ const Character = styled.h4`
   }
 `;
 
+const Arrow = styled.button`
+  background-color: rgba(255, 255, 255, 0.5);
+  border: none;
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 50%;
+  z-index: 1;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+`;
+
 export default function ContentRail({ title, mediaType, data, length }) {
   // Get the row click handler
   const handleRowClick = useHandleRowClick();
@@ -56,43 +78,67 @@ export default function ContentRail({ title, mediaType, data, length }) {
   // Group the titles using GroupJobs
   const groupedTitles = GroupJobs(titleList);
 
+  //Styled Component Carousel
+  const railRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (railRef.current) {
+      railRef.current.scrollBy({ left: -200, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (railRef.current) {
+      railRef.current.scrollBy({ left: 200, behavior: "smooth" });
+    }
+  };
+
   return (
     <Container>
       <Title>{title}</Title>
-      <Rail>
-        {mediaType === "person"
-          ? groupedTitles.map((item, index) => (
-              <Card
-                key={index}
-                onClick={(event) =>
-                  handleRowClick(event, item.media_type, item.id)
-                }
-              >
-                <Image
-                  src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
-                  alt={item.title || item.name}
-                />
-                <Character>
-                  {mediaType === "movie"
-                    ? item.job.join(", ") // Display the aggregated job titles
-                    : item.character}
-                </Character>
-              </Card>
-            ))
-          : titleList.map((item, index) => (
-              <Card
-                key={index}
-                onClick={(event) =>
-                  handleRowClick(event, item.media_type, item.id)
-                }
-              >
-                <Image
-                  src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
-                  alt={item.title || item.name}
-                />
-              </Card>
-            ))}
-      </Rail>
+      <RailContainer>
+        <Arrow onClick={scrollLeft}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </Arrow>
+        <Rail ref={railRef}>
+          {mediaType === "person"
+            ? groupedTitles.map((item, index) => (
+                <Card
+                  key={index}
+                  onClick={(event) =>
+                    handleRowClick(event, item.media_type, item.id)
+                  }
+                >
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
+                    alt={item.title || item.name}
+                  />
+                  <Character>
+                    {mediaType === "movie"
+                      ? item.job.join(", ") // Display the aggregated job titles
+                      : item.character}
+                  </Character>
+                </Card>
+              ))
+            : titleList.map((item, index) => (
+                <Card
+                  key={index}
+                  onClick={(event) =>
+                    handleRowClick(event, item.media_type, item.id)
+                  }
+                >
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
+                    alt={item.title || item.name}
+                  />
+                </Card>
+              ))}
+        </Rail>
+        <Arrow onClick={scrollRight}>
+          {" "}
+          <FontAwesomeIcon icon={faChevronRight} />
+        </Arrow>
+      </RailContainer>
     </Container>
   );
 }
